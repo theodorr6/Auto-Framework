0ac9cff44b8c932f6d1ecd68bcaddec025e0c81c
Ciocotoiu Andrei <ciocotoiuandrei@iMac-Ciocotoiu.local>
2025-08-03 12:53:36 +0300

    Am adaugat metode noi si am luat locatori noi pentru test_register_user

--- autoframework/poms/pages/create_account_page.py
+++ autoframework/poms/pages/create_account_page.py
@@ -8,7 +8,18 @@ class CreateAccountPage(BasePage):
 
 
     ACC_INFO_TITLE = "//b[text()='Enter Account Information']"
+    TITLE = "//label[@for='id_gender1']"
+    PASSWORD_FIELD = "//input[@type='password']"
 
     def verify_acc_info_title(self):
         """Verify the presence of Account Info Title."""
         return self.get_presence_of_element(self.ACC_INFO_TITLE)
+
+
+    def select_title(self):
+        """ Select title"""
+        self.click_element(self.TITLE)
+
+    def type_password(self, password):
+        """ Type password"""
+        self.fill_text(self.PASSWORD_FIELD, password)
---
+++ autoframework/utils.py
@@ -0,0 +1,5 @@
+from autoframework.translate_ro import translations
+
+def translate(word):
+    """ Translate from translate_ro"""
+    return translations[word]
